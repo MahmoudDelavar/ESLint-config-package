414a771ca00b73b087fbad8e5b28fe004c7348d2
mdp3042005 <mahmoud.d.p7@gmail.com>
2025-03-26 19:37:42 +0330

    Add react rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,20 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      js,
+      '@typescript-eslint': tseslint,
+    },
+    extends: ['js/recommended', 'plugin:@typescript-eslint/recommended'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
 ]);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import type { Linter } from 'eslint';
 import { nextJsRules } from './rules';
+import { reactRules } from './rules/reactJsRules';
 
 export const initialConfig = () => {
-  const config = [...nextJsRules] satisfies Linter.Config[];
+  const config = [...nextJsRules, ...reactRules] satisfies Linter.Config[];
   return config;
 };
