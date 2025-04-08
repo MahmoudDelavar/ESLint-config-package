decdcca0000c04f9ef21d54df3fbdf917f0fbd4a
mdp3042005 <mahmoud.d.p7@gmail.com>
2025-04-08 08:28:47 +0330

    Update package.json and tsup.config.ts for ESM and CJS compatibility

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,10 +4,16 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
+  external: ['./worker'],
   format: ['esm', 'cjs'],
   minify: true,
   outDir: 'dist',
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.cjs',
+    };
+  },
   sourcemap: false,
-  splitting: true,
+  splitting: false,
   target: ['es2022', 'node18'],
 });
